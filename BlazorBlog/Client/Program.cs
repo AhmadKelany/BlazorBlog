fc5908943da41c7484eb5137d176ceca00c72166
Ahmad Kelany <ahmad.kelany@yahoo.com>
2023-04-29 13:10:31 +0300

    progress on lesson 2

--- BlazorBlog/Client/Program.cs
+++ BlazorBlog/Client/Program.cs
@@ -1,4 +1,5 @@
 using BlazorBlog.Client;
+using BlazorBlog.Client.Services;
 
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
@@ -11,5 +12,8 @@ builder.Services.AddHttpClient("BlazorBlog.ServerAPI", client => client.BaseAddr
 
 // Supply HttpClient instances that include access tokens when making requests to the server project
 builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("BlazorBlog.ServerAPI"));
+
+
+builder.Services.AddScoped<IBlogService, BlogService>();
 
 await builder.Build().RunAsync();
---
+++ BlazorBlog/Client/Services/IBlogService.cs
@@ -0,0 +1,10 @@
+﻿using BlazorBlog.Shared.Models;
+
+namespace BlazorBlog.Client.Services
+{
+    public interface IBlogService
+    {
+        List<BlogPost> GetBlogPosts();
+        BlogPost GetBlogPostByUrl(string url);
+    }
+}
