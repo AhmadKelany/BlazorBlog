ced8437140714caf798ce45c6bb6407022e07c38
Ahmad Kelany <ahmad.kelany@yahoo.com>
2023-05-06 23:31:25 +0300

    blazor

---
+++ BlazorBlog/Server/Data/BlogsContext.cs
@@ -0,0 +1,14 @@
+﻿using BlazorBlog.Shared.Models;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazorBlog.Server.Data;
+
+public class BlogsContext:DbContext
+{
+    public BlogsContext(DbContextOptions<BlogsContext> options) : base(options)
+    {
+        
+    }
+    public DbSet<BlogPost> BlogPosts { get; set; }
+}
