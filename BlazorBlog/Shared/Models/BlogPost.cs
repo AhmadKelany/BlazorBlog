974bf0590c77d26f1f71b120e623579211f73586
Ahmad Kelany <ahmad.kelany@yahoo.com>
2023-05-14 07:17:09 +0300

    progress on lesson 5

--- BlazorBlog/Shared/Models/BlogPost.cs
+++ BlazorBlog/Shared/Models/BlogPost.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,7 @@ public class BlogPost
 {
     public int Id { get; set; }
     public string Url { get; set; }
+    [Required]
     public string Title { get; set; }
     public string Content { get; set; }
     public string Description { get; set; }
